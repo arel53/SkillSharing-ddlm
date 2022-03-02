b85fe8b32b181cefb651887c9803d2d5eb419f5c
Adam <al394819@uji.es>
2022-03-02 17:45:06 +0100

    Base del proyecto

---
+++ src/main/java/es/uji/ei1027/skillSharing/SkillSharingApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.skillSharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSharingApplication.class, args);
+	}
+
+}
