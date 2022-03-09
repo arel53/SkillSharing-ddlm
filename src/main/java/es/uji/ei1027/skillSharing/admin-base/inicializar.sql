eab625e3ce84c2892e30d001446a2f897633f6ac
arel53 <78787299+arel53@users.noreply.github.com>
2022-03-09 23:28:53 +0100

    adaptado el diseño físico y insert-skill.sql.

--- src/main/java/es/uji/ei1027/skillSharing/admin-base/inicializar.sql
+++ src/main/java/es/uji/ei1027/skillSharing/admin-base/inicializar.sql
@@ -6,71 +6,67 @@ DROP TABLE estudiante;
 
 
 CREATE TABLE estudiante(
-	nif		CHAR(9),
-	nombre		VARCHAR(20) NOT NULL,
-	apellido	VARCHAR(20) NOT NULL,
-	email		VARCHAR(20) NOT NULL,
-	skp		    BOOLEAN NOT NULL,
-	grado		VARCHAR(50) NOT NULL,
-	edad		INTEGER NOT NULL,
-	sexo		VARCHAR(10) NOT NULL,
-	direccion	VARCHAR(50) NOT NULL,
-	horas		INTEGER NOT NULL,
-	CONSTRAINT cp_estudiante PRIMARY KEY (nif),
-	CONSTRAINT calt_estudiante UNIQUE(email)
-	CONSTRAINT ri_estudiante_edad CHECK (edad > 16)
+                           nif		CHAR(9),
+                           nombre		VARCHAR(20) NOT NULL,
+                           apellido	VARCHAR(20) NOT NULL,
+                           email		VARCHAR(20) NOT NULL,
+                           skp		    BOOLEAN NOT NULL,
+                           grado		VARCHAR(50) NOT NULL,
+                           edad		INTEGER NOT NULL,
+                           sexo		VARCHAR(10) NOT NULL,
+                           direccion	VARCHAR(50) NOT NULL,
+                           horas		INTEGER NOT NULL,
+                           CONSTRAINT cp_estudiante PRIMARY KEY (nif),
+                           CONSTRAINT calt_estudiante UNIQUE(email),
+                           CONSTRAINT ri_estudiante_edad CHECK (edad > 16)
 );
 
 CREATE TABLE skill(
-	nombre		VARCHAR(20),
-	activo		BOOLEAN NOT NULL,
-	CONSTRAINT cp_skill PRIMARY KEY (nombre)
-
+                      id_skill    SERIAL PRIMARY KEY,
+                      nombre		VARCHAR(20),
+                      activo		BOOLEAN NOT NULL,
+                      nivel       VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE oferta(
-	id_oferta	NUMERIC(7, 0),
-	estudiante	CHAR(9) NOT NULL,
-	horas		INTEGER NOT NULL,
-	ini_fecha	DATE NOT NULL,
-	fin_fecha	DATE NOT NULL,
-	activa		BOOLEAN NOT NULL,
-	skill		VARCHAR(20) NOT NULL,
-	nivel		VARCHAR(20) NOT NULL,
-	descripcion	VARCHAR(200),
-	CONSTRAINT cp_oferta PRIMARY KEY (id_oferta),
-	CONSTRAINT ca_oferta_sn FOREIGN KEY (skill) REFERENCES skill(nombre) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT ca_oferta_estudiante FOREIGN KEY (estudiante) REFERENCES estudiante(nif) ON DELETE RESTRICT ON UPDATE CASCADE
+                       id_oferta	SERIAL PRIMARY KEY,
+                       estudiante	CHAR(9) NOT NULL,
+                       horas		INTEGER NOT NULL,
+                       ini_fecha	DATE NOT NULL,
+                       fin_fecha	DATE NOT NULL,
+                       activa		BOOLEAN NOT NULL,
+                       id_skill	SERIAL NOT NULL,
+                       nivel		VARCHAR(20) NOT NULL,
+                       descripcion	VARCHAR(200),
+                       CONSTRAINT ca_oferta_sn FOREIGN KEY (id_skill) REFERENCES skill(id_skill) ON DELETE RESTRICT ON UPDATE CASCADE,
+                       CONSTRAINT ca_oferta_estudiante FOREIGN KEY (estudiante) REFERENCES estudiante(nif) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 CREATE TABLE demanda(
-        id_demanda      NUMERIC(7, 0),
-        estudiante      CHAR(9) NOT NULL,
-        horas           INTEGER NOT NULL,
-        ini_fecha       DATE NOT NULL,
-        fin_fecha       DATE NOT NULL,
-        activa          BOOLEAN NOT NULL,
-        skill           VARCHAR(20) NOT NULL,
-        nivel           VARCHAR(20) NOT NULL,
-        descripcion     VARCHAR(200),
-        CONSTRAINT cp_demanda PRIMARY KEY (id_demanda),
-        CONSTRAINT ca_demanda_sn FOREIGN KEY (skill) REFERENCES skill(nombre) ON DELETE RESTRICT ON UPDATE CASCADE,
-        CONSTRAINT ca_demanda_estudiante FOREIGN KEY (estudiante) REFERENCES estudiante(nif) ON DELETE RESTRICT ON UPDATE CASCADE
+                        id_demanda      SERIAL PRIMARY KEY,
+                        estudiante      CHAR(9) NOT NULL,
+                        horas           INTEGER NOT NULL,
+                        ini_fecha       DATE NOT NULL,
+                        fin_fecha       DATE NOT NULL,
+                        activa          BOOLEAN NOT NULL,
+                        id_skill        SERIAL NOT NULL,
+                        descripcion     VARCHAR(200),
+                        CONSTRAINT ca_demanda_sn FOREIGN KEY (id_skill) REFERENCES skill(id_skill) ON DELETE RESTRICT ON UPDATE CASCADE,
+                        CONSTRAINT ca_demanda_estudiante FOREIGN KEY (estudiante) REFERENCES estudiante(nif) ON DELETE RESTRICT ON UPDATE CASCADE
 
 );
 
 
 CREATE TABLE colaboracion(
-	id_oferta	NUMERIC(7,0),
-	id_demanda	NUMERIC(7,0) NOT NULL,
-    ini_fecha       DATE NOT NULL,
-    fin_fecha       DATE  NULL,
-    activa          BOOLEAN NOT NULL,
-	rate		INTEGER NULL,
-	comentario	VARCHAR(200),
-	horas		INTEGER NULL,
-	CONSTRAINT cp_colaboracion PRIMARY KEY (id_oferta, id_demanda),
-	CONSTRAINT ca_colaboracion_id_demanda FOREIGN KEY (id_demanda) REFERENCES demanda(id_demanda) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT ca_colaboracion_id_oferta FOREIGN  KEY (id_oferta) REFERENCES oferta(id_oferta) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT ri_colaboracion_rate CHECK(rate IS NULL OR rate BETWEEN 1 AND 5 )
+                             id_colaboracion	SERIAL PRIMARY KEY,
+                             id_oferta       SERIAL NOT NULL,
+                             id_demanda	    SERIAL NOT NULL,
+                             ini_fecha       DATE NOT NULL,
+                             fin_fecha       DATE  NULL,
+                             activa          BOOLEAN NOT NULL,
+                             rate		INTEGER NULL,
+                             comentario	VARCHAR(200),
+                             horas		INTEGER NULL,
+                             CONSTRAINT ca_colaboracion_id_demanda FOREIGN KEY (id_demanda) REFERENCES demanda(id_demanda) ON DELETE RESTRICT ON UPDATE CASCADE,
+                             CONSTRAINT ca_colaboracion_id_oferta FOREIGN  KEY (id_oferta) REFERENCES oferta(id_oferta) ON DELETE RESTRICT ON UPDATE CASCADE,
+                             CONSTRAINT ri_colaboracion_rate CHECK(rate IS NULL OR rate BETWEEN 1 AND 5 )
 
-);
